034e304accafd2c11e7283d758636e5e0301d002
Kerware <olivier.charles@kerware.com>
2026-01-20 12:30:18 +0100

    Add a new test

--- cypress/e2e/saucedemo.spec.cy.js
+++ cypress/e2e/saucedemo.spec.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Saucedemo authentication spec', () => {
+  
+  it('passes standard user', () => {
     cy.visit('http://www.saucedemo.com')
     cy.get('[data-test="username"]').click();
     cy.get('[data-test="username"]').type('standard_user');
@@ -9,4 +10,17 @@ describe('template spec', () => {
     cy.get('#react-burger-menu-btn').click();
     cy.get('[data-test="logout-sidebar-link"]').click();
   })
-})
+
+
+  it('shows locked out message', () => {
+    cy.visit('http://www.saucedemo.com')
+    cy.get('[data-test="username"]').click();
+    cy.get('[data-test="username"]').type('locked_out_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+  })
+
+
+});
+
